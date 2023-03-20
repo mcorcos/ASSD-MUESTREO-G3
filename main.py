a6fd93bccba98761dab75b6e8af58005c2476009
mcorcos <56644650+mcorcos@users.noreply.github.com>
2023-03-20 20:01:38 -0300

    Primer commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+    Simulador de placa de muestreo
+
+    ASSD: TP1
+    Grupo: 3
+    Integrantes:    ALEJANDRO LESIUK, BRUNO DI SANZO, JUAN PABLO CILFONE ARGIBAY,
+                    LUCÍA IBAÑEZ, MANUEL CORCOS
+
+"""
+
+# Project modules
+from src.app import main
+
+if __name__ == "__main__":
+    main()
