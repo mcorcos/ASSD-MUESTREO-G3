b0e894e6a055848c5f79a7276edba176f7219118
jpcilfone <jcilfoneargibay@itba.edu.ar>
2023-03-26 18:22:48 -0300

    s&h+llave

--- src/cuentas.py
+++ src/cuentas.py
@@ -103,7 +103,7 @@ plt.semilogx(w/(2*pi), mag)    # Bode magnitude plot
 plt.figure()
 plt.semilogx(w/(2*pi), phase)  # Bode phase plot
 plt.show()
-'''
+
 t = np.linspace(0, 0.01, 1000)
 y = np.append(np.zeros(10), np.ones(990))
 y = np.cos(2*pi*1000*t) + np.cos(2*pi*1300*t + 1) 
@@ -115,5 +115,63 @@ fig, ax = plt.subplots()
 ax.plot(t_out, y_out)
 ax.plot(t, y)
 plt.show()
+'''
+
+class sampleAndHold:
+    def __init__(self):
+        self.fs=1e3
+        self.sampledSignal=0
+
+    def updateSH(self, fs):
+        self.fs = fs
+
+    def signalSample(self,y_in,t_in):
+        self.sampledSignal=np.zeros(len(t_in))
+        sampleT=1/self.fs
+        timeInterval=t_in[1]-t_in[0]
+        index=np.rint(sampleT/timeInterval)
+        for i in range(len(t_in)):
+            if i%(index)==0:
+                self.sampledSignal[i]=y_in[i]
+            else:
+                self.sampledSignal[i]=self.sampledSignal[i-1]
+                 
+
+class analogSwitch:
+    def __init__(self):
+        self.fs=1e3
+        self.DC=0.5
+        self.tau=self.DC*1/self.fs
+        self.state=True
+        self.resampledSignal=0
+
+    def updateSwitch(self,DC,fs):
+        self.fs=fs
+        self.DC=DC
+
+    def resampleSignal(self,y_in,t_in):
+        self.resampledSignal=np.zeros(len(t_in))
+        turnOffT=1/self.fs
+        timeInterval=t_in[1]-t_in[0]
+        T_index=np.rint(turnOffT/timeInterval)
+        DC_index=np.rint(turnOffT/timeInterval*self.DC)
+        if self.state:
+            for i in range(len(t_in)):
+                if (i%T_index)<DC_index:
+                    self.resampledSignal[i]=y_in[i]
+                else:
+                    self.resampledSignal[i]=0
+        
 
 
+sandH=sampleAndHold()
+t = np.linspace(0, 0.01, 1000000)
+y = np.cos(2*pi*100*t)
+sandH.signalSample(y,t)
+switch=analogSwitch()
+switch.resampleSignal(sandH.sampledSignal,t)
+fig, ax= plt.subplots()
+ax.plot(t,y)
+ax.plot(t,sandH.sampledSignal)
+ax.plot(t,switch.resampledSignal)
+plt.show()   
